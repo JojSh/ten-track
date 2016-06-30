c977257ab231b78be9ee04d0c009a45e798c34e3
Jojo <joseph.sharratt@gmail.com>
2016-06-30 23:51:56 +0100

    jQuery

---
+++ src/jqfunctionality.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+
+  // $('#hoveringthing').onclick(
+  //   function() {
+  //     $('#disappearingthing').show();
+  //   },
+  //   function() {
+  //     $('#disappearingthing').hide();
+  //   }
+  // );
+
+
+  $("#hoveringthing").click(function() {
+    alert("You clicked the buttross");
+  });
+});
