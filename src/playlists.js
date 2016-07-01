6ac511b904f838051ddb24fa910ae8bbc47a6aad
Jojo <joseph.sharratt@gmail.com>
2016-07-01 14:52:52 +0100

    Extract sample playlist

---
+++ src/playlists.js
@@ -0,0 +1,23 @@
+var samplePlaylist = ["Trevino - L.A. View",
+                      "Gold Panda - Time Eater",
+                       "NT89 - Red Rupture",
+                       "Recondite - Andever",
+                       "Four Tet - Reservoir",
+                       "Whitesquare - Moving Up",
+                       "Midland - Vigilante",
+                       "Drumcell - Conflict Divide",
+                       "Answer Code Request - Meta",
+                       "Shlomo - Escape the Void"
+                      ];
+
+var playlistPics = ["https://i1.sndcdn.com/artworks-000162517285-kcp88z-t500x500.jpg",
+                      "Gold Panda - Time Eater",
+                       "NT89 - Red Rupture",
+                       "Recondite - Andever",
+                       "Four Tet - Reservoir",
+                       "Whitesquare - Moving Up",
+                       "Midland - Vigilante",
+                       "Drumcell - Conflict Divide",
+                       "Answer Code Request - Meta",
+                       "Shlomo - Escape the Void"
+                      ];
--- src/tenTrack.js
+++ src/tenTrack.js
@@ -6,28 +6,32 @@ function TenTrack() {
 TenTrack.prototype.setPlaylist = function(tracks) {
   this.playlist = tracks;
 };
+//
+// var samplePlaylist = ["Trevino - L.A. View",
+//                       "Gold Panda - Time Eater",
+//                        "NT89 - Red Rupture",
+//                        "Recondite - Andever",
+//                        "Four Tet - Reservoir",
+//                        "Whitesquare - Moving Up",
+//                        "Midland - Vigilante",
+//                        "Drumcell - Conflict Divide",
+//                        "Answer Code Request - Meta",
+//                        "Shlomo - Escape the Void"
+//                       ];
 
-var samplePlaylist = ["Trevino - L.A. View",
-                      "Gold Panda - Time Eater",
-                       "NT89 - Red Rupture",
-                       "Recondite - Andever",
-                       "Four Tet - Reservoir",
-                       "Whitesquare - Moving Up",
-                       "Midland - Vigilante",
-                       "Drumcell - Conflict Divide",
-                       "Answer Code Request - Meta",
-                       "Shlomo - Escape the Void"
-                      ];
-
-function display(ary) {
+function display(ary, picAry) {
   var things = "";
-  ary.forEach(function(track) {
-    var thing = ("<li>" + track + "</li>");
+  ary.forEach(function(track, index) {
+    var thing = ("<li>" + track + displayPic(picAry, index) + "</li>");
     console.log(thing);
     things += thing;
   });
   return things;
 }
+
+function displayPic(picAry, index) {
+  return "<img id='disappearingthing' src='picAry[index]'></img>";
+};
 
 function test() {
   console.log("test has run!");
