96bfd49393efa83811ee90c04f6b851e5cfa57a9
Jojo <joseph.sharratt@gmail.com>
2016-07-03 13:12:14 +0100

    Start building hover-index

--- src/tenTrack.js
+++ src/tenTrack.js
@@ -7,21 +7,28 @@ TenTrack.prototype.setPlaylist = function(tracks) {
   this.playlist = tracks;
 };
 
-function display(ary, picAry) {
-  var things = "";
+function display(ary) {
+  var element = "";
   ary.forEach(function(track, index) {
-    var thing = ("<li>" + track + displayPic(picAry, index) + "</li>");
-    console.log(thing);
-    things += thing;
+    var components = ("<p>" + track + "</p>");
+    element += components;
   });
-  return things;
+  return element;
 }
 
 function displayPic(picAry, index) {
+  // return picAry[index];
   return "<img id='disappearingthing' src='" + picAry[index] + "'></img>";
 };
 
 function test() {
   console.log("test has run!");
   return (2 * 7);
+}
+
+function forLoop(ary) {
+  for (var i = 0; i < ary.length; i++ ) {
+    console.log(i);
+    return i;
+  }
 }
