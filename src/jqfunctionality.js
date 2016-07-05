e34774e2fb0b8fbdce2b297ecee5859691d9ee1c
Jojo <joseph.sharratt@gmail.com>
2016-07-05 16:03:45 +0100

    Highlight selected track

--- src/jqfunctionality.js
+++ src/jqfunctionality.js
@@ -1,26 +1,58 @@
 $(document).ready(function() {
 
-  $('#disappearingthing').hide();
+  var clicked = false;
 
-  // $('#track0').hover(function() {
-  //     $('#disappearingthing').show();
-  //   },
-  //   function() {
-  //     $('#disappearingthing').hide();
-  //   }
-  // );
+  // hideIfNotClicked('#disappearingthing');
+
 
   $('#tracks p').hover(function() {
-      var img = $('#disappearingthing');
-      var id = $(this).attr('data-number');
-      img.attr('src', playlistPics[id]);
-      img.show();
-      // $('#disappearingthing').show();
+    setImgSrc(this, '#disappearingthing');
     },
     function() {
-      $('#disappearingthing').hide();
+      hideIfNotClicked('#disappearingthing');
     }
   );
 
+  function hideIfNotClicked(pic) {
+    if (clicked === false) {
+      $(pic).hide();
+    }
+  }
+
+
+  $('#tracks p').click(function(){
+    clicked = true;
+    highlightTrack(this);
+    console.log('Clicking element');
+    setImgSrc(this, '#pics');
+
+  });
+
+  function highlightTrack(element) {
+    $(element).css({
+      "color": "#69b3bc",
+      "text-decoration": "underline"
+    });
+  }
+
+  function setImgSrc(element, divID) {
+    var img = $(divID);
+    var id = $(element).attr('data-number');
+    img.attr('src', playlistPics[id]);
+    img.show();
+  }
 
 });
+
+function display(ary) {
+  var element = "";
+  ary.forEach(function(track, index) {
+    var components = ("<p data-number="+index+">" + recordPic() +" "+ track + "</p>");
+    element += components;
+  });
+  return element;
+}
+
+function recordPic() {
+  return "<img src='styling/images/record_icon_small.png' ></img>";
+}
--- src/tenTrack.js
+++ src/tenTrack.js
@@ -5,25 +5,3 @@ function TenTrack() {
 TenTrack.prototype.setPlaylist = function(tracks) {
   this.playlist = tracks;
 };
-
-function display(ary) {
-  var element = "";
-  ary.forEach(function(track, index) {
-    var components = ("<p data-number="+index+">" + track + "</p>");
-    element += components;
-  });
-  return element;
-}
-
-function displayPic(picAry, index) {
-  // return picAry[index];
-  return "<img id='disappearingthing' src='" + picAry[index] + "'></img>";
-}
-
-
-function forLoop(ary) {
-  for (var i = 0; i < ary.length; i++ ) {
-    console.log(i);
-    return i;
-  }
-}
