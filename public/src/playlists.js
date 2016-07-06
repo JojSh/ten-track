de6d8d022cdc0782497ed34d17118a71e5c08a2d
Jojo <joseph.sharratt@gmail.com>
2016-07-06 15:48:03 +0100

    Style + img src changes

--- public/src/playlists.js
+++ public/src/playlists.js
@@ -14,7 +14,7 @@ var playlistPics = ["https://i1.sndcdn.com/artworks-000162517285-kcp88z-t500x500
                       "https://i1.sndcdn.com/artworks-000148194161-zh77v7-t500x500.jpg",
                        "https://i1.sndcdn.com/artworks-000161737209-cjkz58-t500x500.jpg",
                        "https://i1.sndcdn.com/artworks-000166507496-rw6tha-t500x500.jpg",
-                       "styling/images/four_tet_reservoirs.png",
+                       "../public/styling/images/four_tet_reservoirs.png",
                        "https://i1.sndcdn.com/artworks-000165227595-oe3jzz-t500x500.jpg",
                        "https://i1.sndcdn.com/artworks-000165212860-214e2n-t500x500.jpg",
                        "https://i1.sndcdn.com/artworks-000146463684-hrtcgx-t500x500.jpg",
@@ -29,7 +29,7 @@ var trackPlayers = ["https://w.soundcloud.com/player/?url=https%3A//api.soundclo
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/247860480&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/262623246&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/270788368&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
-                    "Four Tet - Reservoir",
+                    "https://www.youtube.com/embed/fNWHvuCrvac",
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/266764857&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/266750035&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
                     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/246172258&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false",
@@ -38,6 +38,6 @@ var trackPlayers = ["https://w.soundcloud.com/player/?url=https%3A//api.soundclo
                     ];
 
 function chopScUrl(string) {
-  var chop = string.split('src="')
-  return (chop[1].split('"><'))[0]
+  var chop = string.split('src="');
+  return (chop[1].split('"><'))[0];
 }
